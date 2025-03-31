674f0c5aa24f2065591ea4464b4cc78223fb7537
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2025-03-31 11:04:02 +0530

    routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,27 @@
 import React from "react";
 
+const modules = import.meta.glob<{ default: React.ComponentType<any> }>(
+    "./pages/**/*.tsx"
+);
 
-const modules = port MediaMetadata.
+const wrapWithLayout = (Component: any, Layout: any) => {
+    return React.createElement(
+        React.Suspense,
+        {
+            fallback: React.createElement("div", null, "loading"),
+        },
+        Layout
+            ? React.createElement(Layout, null, React.createElement(Component))
+            : React.createElement(Component)
+    );
+};
+
+const routes = Object.keys(modules).map((path) => {
+    const routePath = path
+
+        .replace("./pages", "")
+        .replace(/\/index\.tsx$/, "/")
+        .replace(/\[([^\]]+)]/g, ":$1");
+
+    const layoutPath = path.split();
+});
