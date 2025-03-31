3e938fcdf77613fbd5a760122c4208040776aef7
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2025-03-31 13:00:17 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
-import routes from "./routes";
+import routes from "./router/routes";
 
 const AppRoutes = () => {
     return (
